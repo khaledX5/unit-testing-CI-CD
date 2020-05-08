df7adf5e56b3c6f7cf51d1d8a3bba4f0bcb69d15
khaledx52 <K.karam@sol-era.com>
2020-05-09 00:25:28 +0200

    rehande the code

--- test/square.test.js
+++ test/square.test.js
@@ -1,9 +1,14 @@
 const square_service = require('../services/squareService');
 
-test('get square area if length/width equal 10 the area will be equal 100', () => {
-  let body = {
-    "length":20,
-    "width":20
-  };
-  expect(square_service.calculateArea(body)).toBe(400);
-});
+test('get square area if length/width equal 10 the area will be equal 100',getSquareArea );
+
+
+
+
+function getSquareArea(){
+    let body = {
+      "length":20,
+      "width":20
+    };
+    expect(square_service.calculateArea(body)).toBe(400);
+}
