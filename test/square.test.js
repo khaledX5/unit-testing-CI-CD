9109a910ba6984b96c61a8334c3b5d9388cbc5a6
khaledx52 <K.karam@sol-era.com>
2020-05-09 01:45:15 +0200

    push some with error

--- test/square.test.js
+++ test/square.test.js
@@ -10,5 +10,5 @@ function getSquareArea(){
       "length":20,
       "width":20
     };
-    expect(square_service.calculateArea(body)).toBe(400);
+    expect(square_service.calculateArea(body)).toBe(10);
 }
